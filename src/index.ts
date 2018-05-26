972914d75a5bb6b3b94d7bfbf2d937d0cdbfdef1
Tanner Goins <tannergz28@gmail.com>
2018-05-26 15:17:36 -0500

    add prettier, semicolons, reformat

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,33 @@
-import { Message } from 'discord.js'
-import { CommandoClient } from 'discord.js-commando'
-import { Config } from '@natsuki/util'
-import { onMessage } from './lib/Events'
+import { Message } from 'discord.js';
+import { CommandoClient } from 'discord.js-commando';
+import { Config } from '@natsuki/util';
+import { onMessage } from './lib/Events';
 
 export class Module {
-  static config: Config
-  static client: CommandoClient
-  static id = 'Levels'
-  static description = 'A leveling system that awards XP when users send messages. Also rewards credits when a user levels up.'
+  static config: Config;
+  static client: CommandoClient;
+  static id = 'Levels';
+  static description = 'A leveling system that awards XP when users send messages. Also rewards credits when a user levels up.';
 
   /**
    * Initializes module
    * @param client
    * @param config
    */
-  public async init (client: CommandoClient, config: Config) {
-    Module.client = client
-    Module.config = config
-    await this.registerListeners(client, config)
+  public async init(client: CommandoClient, config: Config) {
+    Module.client = client;
+    Module.config = config;
+    await this.registerListeners(client, config);
   }
 
   /**
    * Register events
    * @param client
    */
-  private async registerListeners (client: CommandoClient, config: Config): Promise<void> {
-    client.on('message', message => onMessage(message, config))
+  private async registerListeners(
+    client: CommandoClient,
+    config: Config
+  ): Promise<void> {
+    client.on('message', (message) => onMessage(message, config));
   }
 }
--- src/lib/Events.ts
+++ src/lib/Events.ts
@@ -1,40 +1,50 @@
-import { Message, TextChannel } from 'discord.js'
-import axios from 'axios'
-import { Module } from '../'
-import { Logger, MessageUtility, Config } from '@natsuki/util'
-import { User as NatsukiUser } from '@natsuki/db'
-import { giveXp } from './'
+import { Message, TextChannel } from 'discord.js';
+import axios from 'axios';
+import { Module } from '../';
+import { Logger, MessageUtility, Config } from '@natsuki/util';
+import { User as NatsukiUser } from '@natsuki/db';
+import { giveXp } from './';
 
 export const onMessage = async (message: Message, config: Config) => {
-  if (message.author.bot || !message.content ||
-    !message.content.trim() || message.channel.type !== 'text') {
-    return
+  if (
+    message.author.bot ||
+    !message.content ||
+    !message.content.trim() ||
+    message.channel.type !== 'text'
+  ) {
+    return;
   }
 
-  const { api } = Module.config
-  const baseRoute = `${api.address}/users`
+  const { api } = Module.config;
+  const baseRoute = `${api.address}/users`;
 
   // Prevent the user from earning xp for bot commands.
   // Handles *most* bots.
-  const firstTwoMatch = message.content.trim().substring(0, 2).match(/[a-z]/ig)
+  const firstTwoMatch = message.content
+    .trim()
+    .substring(0, 2)
+    .match(/[a-z]/gi);
   if (!firstTwoMatch || firstTwoMatch.length !== 2) {
-    return
+    return;
   }
 
-  const route = `${baseRoute}/${message.author.id}?token=${api.token}`
+  const route = `${baseRoute}/${message.author.id}?token=${api.token}`;
 
-  axios.get(route)
-    .then(res => {
+  axios
+    .get(route)
+    .then((res) => {
       if (!res.data) {
-        MessageUtility.createUser(message.author, Module.config).catch(Logger.error)
-        return
+        MessageUtility.createUser(message.author, Module.config).catch(
+          Logger.error
+        );
+        return;
       }
 
-      const user = res.data as NatsukiUser
+      const user = res.data as NatsukiUser;
 
-      giveXp(user, message).catch(Logger.error)
+      giveXp(user, message).catch(Logger.error);
     })
     .catch((err: any) => {
-      Logger.error(err)
-    })
-}
+      Logger.error(err);
+    });
+};
